0caa98a64156dee21cbe6bc94301da79ed763dc9
raulandrez <raulandrez@gmail.com>
2015-05-16 20:02:15 -0500

    Prueba Nro 2

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        echo 'esto es una prueba..... ';
+        
         ?>
     </body>
 </html>
